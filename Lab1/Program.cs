ce9a4d1ff035c35d45ee34a40bac7517aef08f8e
Jakub Czechowicz <w69932@student.wsiz.edu.pl>
2024-10-17 21:57:22 +0200

    zmiana

---
+++ Lab1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab1.Constructions;
+
+Console.WriteLine("Hello, World!");
+var constaction1 = new Construction();
+var constaction2 = new Construction(23, 45, 4, 4, "Material");
+Console.WriteLine("Hello World!");
