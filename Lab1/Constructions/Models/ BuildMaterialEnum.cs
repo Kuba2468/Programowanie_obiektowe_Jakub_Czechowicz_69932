85ffd2cc9cd7e0a2e41b133103a84be516b8885a
Jakub Czechowicz <w69932@student.wsiz.edu.pl>
2024-10-31 11:17:56 +0100

    zmiana3

---
+++ Lab1/Constructions/Models/ BuildMaterialEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace Lab1.Construction.Models
+{
+    internal enum BuildMaterialEnum
+    {
+        brik = 0,
+        Wood = 0,
+        Concrete = 200,
+    }
+}
